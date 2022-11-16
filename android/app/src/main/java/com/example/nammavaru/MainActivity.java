02dfa15c6d923218cbd0aab31bbdfe46d011eac3
naveen-kc <naveen@active.agency>
2022-11-16 18:34:11 +0530

    made proper

--- android/app/src/main/java/com/example/nammavaru/MainActivity.java
+++ android/app/src/main/java/com/example/nammavaru/MainActivity.java
@@ -21,43 +21,43 @@ public class MainActivity extends FlutterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //GeneratedPluginRegistrant.registerWith(MainActivity.this);
-        new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
-                new MethodChannel.MethodCallHandler() {
-                    @Override
-                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-                        if (call.method.equals("launchUpi")) {
-                            Uri uri = Uri.parse(call.argument("url").toString());
-                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-
-                            if (intent.resolveActivity(getPackageManager()) != null) {
-                                startActivityForResult(intent,1);
-                            } else {
-                                Toast.makeText(MainActivity.this, "No UPI apps found in your device!", Toast.LENGTH_SHORT).show();
-                            }
-
-                            Log.d("Call", "onMethodCall: launchUpi");
-                            callResult = result;
-                        } else {
-                            result.notImplemented();
-                        }
-                    }
-                });
+//        new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+//                new MethodChannel.MethodCallHandler() {
+//                    @Override
+//                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+//                        if (call.method.equals("launchUpi")) {
+//                            Uri uri = Uri.parse(call.argument("url").toString());
+//                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//
+//                            if (intent.resolveActivity(getPackageManager()) != null) {
+//                                startActivityForResult(intent,1);
+//                            } else {
+//                                Toast.makeText(MainActivity.this, "No UPI apps found in your device!", Toast.LENGTH_SHORT).show();
+//                            }
+//
+//                            Log.d("Call", "onMethodCall: launchUpi");
+//                            callResult = result;
+//                        } else {
+//                            result.notImplemented();
+//                        }
+//                    }
+//                });
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        Log.d("Result","Dataa :"+data.getData());
-        if(data!=null && callResult!=null) {
-            String res = data.getStringExtra("response");
-            Log.d("Response","respi :"+res);
-
-                callResult.success(res);
-
-        }else{
-            Log.d("Result","Data = null (User canceled)");
-            callResult.success("User Cancelled");
-        }
-        super.onActivityResult(requestCode,resultCode,data);
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        Log.d("Result","Dataa :"+data.getData());
+//        if(data!=null && callResult!=null) {
+//            String res = data.getStringExtra("response");
+//            Log.d("Response","respi :"+res);
+//
+//                callResult.success(res);
+//
+//        }else{
+//            Log.d("Result","Data = null (User canceled)");
+//            callResult.success("User Cancelled");
+//        }
+//        super.onActivityResult(requestCode,resultCode,data);
+//    }
 }
